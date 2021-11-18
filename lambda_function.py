bb25cd185ae85e0e7fa7ad62b5e4bb775d1a620e
Praveen Anchi <praveen.anchi@yara.com>
2021-11-18 12:26:39 +0530

    adding changes to lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def lambda_handler(event,context):
 
-    d={'call':[1,2,3],'call2':[4,5,6]}
+    d={'call':[1,2,3],'call2':[4,5,6],'cal3':[5,6,7]}
     df=pd.DataFrame(data=d)
     print(df)
     print("Done x1")
