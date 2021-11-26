d23fc06f9e7f70ee224d0b9221c84f37ed472d9a
Praveen Anchi <praveen.anchi@yara.com>
2021-11-26 16:35:50 +0530

    added function to lambda

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,6 @@ import uuid
 from urllib.parse import unquote_plus
 import pandas as pd
 def lambda_handler(event, context):
-  data={'a':[1,2,3],'b':[4,5,6]}
+  data={'a':[1,2,3],'b':[4,5,6],'c':[78,4,5]}
   df=pd.DataFrame(data)
   print(df)
